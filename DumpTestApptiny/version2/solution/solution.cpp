db3ec51721335499383be8ffadc4b806721fa939
unknown <pixma38@gmail.com>
2014-07-18 11:06:09 +0530

    initial commit .

---
+++ DumpTestApptiny/version2/solution/solution.cpp
@@ -0,0 +1,17 @@
+/*
+ * solution.cpp
+ *
+ * Created: 7/18/2014 11:04:27 AM
+ *  Author: annim
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
